95f43a1fc46d05fa9f07dd6b8a73ba716aa2f6e4
Daniel <dje1g20@soton.ac.uk>
2023-01-30 19:11:52 +0000

    init comit2

---
+++ sqlite/databases/populate_mydatabase.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+contents=$(cat gistfile1.txt);
+file=gistfile1.txt;
+
+#echo file;
+#echo $file;
+#echo $contents;
+
+
+#cat $file | grep ' '
+# | cut -d '{' -f 1;
+
+lines = slpit file;
+echo $lines;
+#for line in $contents; do
+#	echo "$line"
+#done
+
+
+
